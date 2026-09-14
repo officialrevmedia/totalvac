6e23563497abb6d402fca6d73bec6cf17b8322c0
officialrevmedia <info@revmedia.ca>
2026-09-14 13:41:12 -0400

    Add files via upload

--- docs/assets/js/site-config.js
+++ docs/assets/js/site-config.js
@@ -2,5 +2,10 @@ window.TOTALVAC_CONFIG = {
   "formEndpoint": null,
   "phone": "(437) 908-2330",
   "email": "totalvacsolutions@gmail.com",
-  "analyticsId": null
+  "analyticsId": null,
+  "googleAds": {
+    "tagId": "AW-18342776758",
+    "phoneClickLabel": null,
+    "formSubmitLabel": null
+  }
 };
